459c5068275e28622e9cf6784bf511ddfaaf1d28
chris <c.caressimo@gmail.com>
2021-05-13 21:30:08 -0400

    setting buttons and iframe sizes

--- client/js/script.js
+++ client/js/script.js
@@ -1,4 +1,7 @@
 const { response } = require("express")
 
-
+function setHalfVolume() {
+  var myAudio = document.getElementById("playlist_generator");
+  myAudio.volume = 0.5; //Changed this to 0.5 or 50% volume since the    function is called Set Half Volume ;)
+}
 
